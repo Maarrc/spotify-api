5c46cac3eb2c4b3ff4b838ddb3a768201ea98b0d
marc <marcverdeguertierno@gmail.com>
2024-01-14 23:51:44 +0100

    idioma, tipo y canciones

--- src/Controller/CancionesController.php
+++ src/Controller/CancionesController.php
@@ -2,13 +2,78 @@
 
 namespace App\Controller;
 
+use App\Entity\AnyadeCancionPlaylist;
+use App\Entity\Cancion;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class CancionesController extends AbstractController
 {
-    public function index()
+    public function canciones(Request $request, SerializerInterface $serializer)
     {
-        return new Response('Hello!');
+        if ($request->isMethod('GET')) {
+            $canciones = $this ->getDoctrine()->getRepository(Cancion::class)->findAll();
+            $canciones = $serializer->serialize(
+                $canciones,
+                'json',
+                ['groups'=>['Cancion']]
+
+            );
+            return new Response($canciones);
+        }
+    }
+
+    public function cancion(Request $request, SerializerInterface $serializer)
+    {
+        $id = $request->get('id');
+        if ($request->isMethod('GET')) {
+            $cancion = $this ->getDoctrine()->getRepository(Cancion::class)->findOneBy(['id'=> $id]);
+            $cancion = $serializer->serialize(
+                $cancion,
+                'json',
+                ['groups'=>['Cancion']]
+
+            );
+            return new Response($cancion);
+        }
+    }
+
+    public function canciones_playlist(Request $request, SerializerInterface $serializer)
+    {
+        $idPlaylist = $request->get('id');
+        if ($request->isMethod('GET')) {
+            $playlist = $this ->getDoctrine()->getRepository(AnyadeCancionPlaylist::class)->findBy(['playlist'=> $idPlaylist]);
+            $idCanciones = [];
+            foreach ($playlist as $play) {
+                $idCanciones [] = $play->getCancion();
+            }
+            $canciones = [];
+
+            foreach($idCanciones as $cancion) {
+                $can = $this ->getDoctrine()->getRepository(Cancion::class)->findOneBy(['id'=> $cancion]);
+                $canciones [] = $can;
+                
+            }
+            $canciones = $serializer->serialize(
+                $canciones,
+                'json',
+                ['groups'=>['Cancion']]
+
+            );
+            return new Response($canciones);
+        }
+    }
+
+    public function cancion_playlist(Request $request, SerializerInterface $serializer)
+    {
+        if ($request->isMethod('POST')) {
+
+        }
+
+        if ($request->isMethod('DELETE')) {
+            
+        }
     }
 }
--- src/Controller/IdiomaController.php
+++ src/Controller/IdiomaController.php
@@ -2,13 +2,25 @@
 
 namespace App\Controller;
 
+use App\Entity\Idioma;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class IdiomaController extends AbstractController
 {
-    public function index()
+    public function idioma(Request $request, SerializerInterface $serializer)
     {
-        return new Response('Hello!');
+        if ($request->isMethod('GET')) {
+            $idiomas = $this ->getDoctrine()->getRepository(Idioma::class)->findAll();
+            $idiomas = $serializer->serialize(
+                $idiomas,
+                'json',
+                ['groups'=>['Idioma']]
+
+            );
+            return new Response($idiomas);
+        }
     }
 }
