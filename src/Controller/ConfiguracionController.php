dd15ba9d053299ec4bf6404c1f94084983908230
DEYYF <unaifago@gmail.com>
2024-01-16 13:51:43 +0100

    Configuraciones acabada y playlist casi terminada

--- src/Controller/ConfiguracionController.php
+++ src/Controller/ConfiguracionController.php
@@ -2,10 +2,59 @@
 
 namespace App\Controller;
 
+use App\Entity\Configuracion;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class ConfiguracionController extends AbstractController
 {
+    public function usuario_configuracion(Request $request, SerializerInterface $serializer)
+    {
+        if ($request->isMethod('GET')){
+            $id_usuario = $request->get('usuario_id');
+            
+            $configuracion_usuario = $this->getDoctrine()->getRepository(Configuracion::class)->findBy(['usuario'=>$id_usuario]);
+
+            $configuracion_usuario = $serializer->serialize(
+                $configuracion_usuario,
+                'json',
+                ['groups'=>['Configuraciones', 'Usuario', 'Calidad', 'Idioma', 'TipoDescarga']]
+
+            );
+            return new Response($configuracion_usuario);
+        
+
+        }
+
+        if ($request->isMethod('PUT')){
+            $id_usuario = $request->get('usuario_id');
+            $configuracion_usuario = $this->getDoctrine()->getRepository(Configuracion::class)->findBy(['usuario'=>$id_usuario]);
+
+            if (!empty($configuracion_usuario)){
+                $bodydata = $request->getContent();
+                $configuracion_usuario = $serializer->deserialize(
+                    $bodydata,
+                    Configuracion::class,
+                    'json',
+                    ['object_to_populate' => $configuracion_usuario ]
+                );
+                $this->getDoctrine()->getManager()->persist($configuracion_usuario);
+                $this->getDoctrine()->getManager()->flush();
+
+                $configuracion_usuario = $serializer->serialize(
+                    $configuracion_usuario,
+                    'json',
+                    ['groups'=>['Configuraciones', 'Usuario', 'Calidad', 'Idioma', 'TipoDescarga']]
+    
+                );
+
+                return new Response($configuracion_usuario);
+            }
+            return new JsonResponse(['msg'=> 'usuario not found'],404);
+        }
+    }
 
 }
--- src/Controller/PlaylistsController.php
+++ src/Controller/PlaylistsController.php
@@ -2,13 +2,110 @@
 
 namespace App\Controller;
 
+use App\Entity\Eliminada;
+use App\Entity\Playlist;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class PlaylistsController extends AbstractController
 {
-    public function index()
+    public function playlists(Request $request, SerializerInterface $serializer)
     {
-        return new Response('Hello!');
+
+        if($request->isMethod('GET'))
+        {
+            $playlists = $this->getDoctrine()->getRepository(Playlist::class)->findAll();
+
+            $playlists = $serializer->serialize(
+                $playlists,
+                'json',
+                ['groups'=>['Playlist']]
+
+            );
+            return new Response($playlists);
+        }
+
+        if($request->isMethod('POST'))
+        {
+
+        }
+        
+    }
+
+
+    public function playlists_usuario(Request $request, SerializerInterface $serializer)
+    {
+
+        if($request->isMethod('GET'))
+        {
+            $id_usuario = $request->get('id');
+            $playlists = $this->getDoctrine()->getRepository(Playlist::class)->findBy(['usuario'=>$id_usuario]);
+
+            $playlists = $serializer->serialize(
+                $playlists,
+                'json',
+                ['groups'=>['Playlist', 'Usuario']]
+
+            );
+            return new Response($playlists);
+
+        }
+        
+
+    }
+
+    public function playlist(Request $request, SerializerInterface $serializer)
+    {
+        if($request->isMethod('GET'))
+        {   
+            $id = $request->get('id');
+            $playlist = $this->getDoctrine()->getRepository(Playlist::class)->findOneBy(['id'=>$id]);
+            
+            $playlist = $serializer->serialize(
+                $playlist,
+                'json',
+                ['groups'=>['Playlist']]
+
+            );
+            return new Response($playlist);
+        }
+    }
+
+
+    public function playlist_usuario (Request $request, SerializerInterface $serializer)
+    {
+        $id_usuario = $request->get('id');
+        $id_playlist = $request->get('id_playlist');
+
+        if($request->isMethod('GET'))
+        {
+            $playlist = $this->getDoctrine()->getRepository(Playlist::class)->findBy(['id'=>$id_playlist, 'usuario' => $id_usuario]);
+        
+            $playlist = $serializer->serialize(
+                $playlist,
+                'json',
+                ['groups'=>['Playlist', 'Usuario']]
+
+            );
+            return new Response($playlist);
+
+        }
+
+        if($request->isMethod('DELETE'))
+        {
+            $playlist = $this->getDoctrine()->getRepository(Playlist::class)->findOneBy(['id'=>$id_playlist, 'usuario' => $id_usuario]); 
+            $hoy = getdate();
+
+
+
+            $this->getDoctrine()->getManager(Eliminada::class)->persist($hoy, $playlist);
+            $this->getDoctrine()->getManager(Eliminada::class)->flush();
+
+            
+        
+        
+        }
     }
 }
